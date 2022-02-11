143cf5a8669cc6b1def8f8f9fe61c6fa4279988f
Dimitri Chakma <dimitrichakma18@gmail.com>
2022-02-11 21:39:28 +0600

    Router Added

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import Menu from './Menu'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import { Route, Routes, Navigate } from 'react-router-dom'
+
 const Body = () => {
+
     return (
         <div>
-            <Menu />
+            <Routes>
+                <Route path='/' exact element={<Home />} />
+                <Route path='/menu' exact element={<Menu />} />
+                <Route path='/about' exact element={<About />} />
+                <Route path='/contact' exact element={<Contact />} />
+                <Route path='/home' element={<Navigate replace to='/' />} />
+
+
+            </Routes>
+
+
+
 
         </div>
 
